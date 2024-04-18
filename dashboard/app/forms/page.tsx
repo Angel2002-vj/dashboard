8f1b3523da83d3226480cf4952b482af91db3ce5
Angel <vjangel953@gmail.com>
2024-04-18 16:25:33 +0530

    nav bar created .

---
+++ dashboard/app/forms/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Forms = () => {
+  return (
+    <div className='container-main'>
+       <h1 className="text-orange-600 mt-11 text-2xl">Forms</h1>
+    </div>
+  )
+}
+
+export default Forms
