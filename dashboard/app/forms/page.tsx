71274215394bb140dfcab1641becbd190442ecf7
Angel <vjangel953@gmail.com>
2024-04-19 11:32:22 +0530

    nav bar created .

--- dashboard/app/forms/page.tsx
+++ dashboard/app/forms/page.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Forms = () => {
   return (
     <div className='container-main'>
        <h1 className="text-orange-600 mt-11 text-2xl">Forms</h1>
+
+
+      
     </div>
   )
 }
